f7ba772817f2119af037eb991b19c9742315884b
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-07 16:45:55 -0700

    Implement logic for playing a single round

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function getComputerChoice() {
   return CHOICES[choiceIndex];
 }
 
+function playRound(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toLowerCase();
+
+  switch (true) {
+    case playerChoice === 'rock' && computerSelection === 'scissors':
+    case playerChoice === 'paper' && computerSelection === 'rock':
+    case playerChoice === 'scissors' && computerSelection === 'paper':
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+      break;
+    case playerChoice === computerSelection:
+      return `Tie! You both chose ${computerSelection}`;
+      break;
+    default:
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
